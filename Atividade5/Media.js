09b2c5f82fd5d73baf44bac7493b919f5e148a14
borgato <borgato_@hotmail.com>
2021-03-17 22:36:00 -0300

    Add files via upload

---
+++ Atividade5/Media.js
@@ -0,0 +1,14 @@
+var num1 = prompt("Digite o primeiro valor:");
+parseFloat(num1);
+var num2 = prompt("Digite o segundo valor:");
+parseFloat(num2);
+var num3 = prompt("Digite o terceiro valor:");
+parseFloat(num3);
+    
+media = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3))/3;
+
+var resultado = media;
+        
+alert ("A média dos valores informados é: "+resultado);
+
+
---
+++ Atividade5/Operações.js
@@ -0,0 +1,16 @@
+var num1 = prompt("Digite o primeiro valor:");
+var num2 = prompt("Digite o segundo valor:");
+    
+soma = parseFloat(num1) + parseFloat(num2);
+
+subtracao = parseFloat(num1) - parseFloat(num2);
+
+produto = parseFloat(num1) * parseFloat(num2);
+
+divisao = parseFloat(num1) / parseFloat(num2);
+
+resto = parseFloat(num1) % parseFloat(num2);
+        
+alert (" A soma dos valores informados é: "+soma+"\n A subtração do primeiro pelo segundo é: "+subtracao+"\n O produto dos dois números é: "+produto+"\n A divisão entre eles é: "+divisao+"\n O resto da divisão é: "+resto);
+
+
