baa9d4a687f7891eb8134d441d09ff8169e905fb
borgato <borgato_@hotmail.com>
2021-04-07 20:48:23 -0300

    Add files via upload

---
+++ Atividade7/maiorde3.js
@@ -0,0 +1,6 @@
+var num1 = parseFloat(prompt("Digite um número:"));
+var num2 = parseFloat(prompt("Digite um número:"));
+var num3 = parseFloat(prompt("Digite um número:"));
+
+alert("O maior número informado é: "+ Math.max(num1, num2, num3));
+
