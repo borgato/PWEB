08fc31abbe0d2521092f4a49b7de17efaa763315
borgato <borgato_@hotmail.com>
2021-04-28 21:45:50 -0300

    Add files via upload

---
+++ Atividade10/janelas.js
@@ -0,0 +1,19 @@
+function ler() {
+	alert("Abra a janela");
+}
+		
+function abrir(obj) {
+    document.getElementById("janela").src="janelaaberta.png";
+}
+
+function fechar(obj) {
+    document.getElementById("janela").src="janelafechada.png";
+}
+
+function quebrar(obj) {
+    document.getElementById("janela").src="janelaquebrada.png";
+}
+
+function mover(obj) {
+    document.getElementById("janela").src="janelaaberta.png";
+}
